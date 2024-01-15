d57d244a375c56f8bf1432fe4fd0ef1370c1625b
djowcoco <156693190+djowcoco@users.noreply.github.com>
2024-01-15 16:57:29 +0100

    Add files via upload

---
+++ Dictionaire Pokemon.py
@@ -0,0 +1,8 @@
+#disctionnaire des pokemon des 9 premieres générations
+#classification par nom, type, région, génération
+pokemon = { 
+pokemons[4] = {'nom': 'Salamèche', 'type': 'Feu'}
+pokemons[5] = {'nom': 'Reptincel', 'type': 'Feu'}
+pokemons[6] = {'nom': 'Dracaufeu', 'type': 'Feu'}
+# Continuez à ajouter des Pokémon ici
+}
